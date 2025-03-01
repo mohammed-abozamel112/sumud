909de6ac5ad7192005bc444980910c49ba8f1d5f
mohammed.abozamel112 <mohammed.abozamel112@gmail.com>
2025-03-02 01:52:52 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 AOS.init({ duration: 1000 });
-<script>
-  function trackEvent(eventCategory, eventAction, eventLabel) {
-    window.dataLayer = window.dataLayer || [];
-    dataLayer.push({
-      'event': 'custom_event',
-      'eventCategory': eventCategory,
-      'eventAction': eventAction,
-      'eventLabel': eventLabel
-    });
-    console.log('Event pushed:', eventCategory, eventAction, eventLabel);
-  }
-</script>
+function trackEvent(eventCategory, eventAction, eventLabel) {
+  window.dataLayer = window.dataLayer || [];
+  dataLayer.push({
+    event: "custom_event",
+    eventCategory: eventCategory,
+    eventAction: eventAction,
+    eventLabel: eventLabel,
+  });
+  console.log("Event pushed:", eventCategory, eventAction, eventLabel);
+}
