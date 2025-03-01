8e74e8c9c0e5d725a3d06e20e119ed490726b251
mohammed.abozamel112 <mohammed.abozamel112@gmail.com>
2025-03-02 01:48:22 +0300

    new fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 AOS.init({ duration: 1000 });
+<script>
+  function trackEvent(eventCategory, eventAction, eventLabel) {
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+      'event': 'custom_event',
+      'eventCategory': eventCategory,
+      'eventAction': eventAction,
+      'eventLabel': eventLabel
+    });
+    console.log('Event pushed:', eventCategory, eventAction, eventLabel);
+  }
+</script>
