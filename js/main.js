3c9de0939fceab643ec42ce58e610d81f44bc9e1
mohammed.abozamel112 <mohammed.abozamel112@gmail.com>
2025-03-02 01:56:19 +0300

    yy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-AOS.init({ duration: 1000 });
-function trackEvent(eventCategory, eventAction, eventLabel) {
-  window.dataLayer = window.dataLayer || [];
-  dataLayer.push({
-    event: "custom_event",
-    eventCategory: eventCategory,
-    eventAction: eventAction,
-    eventLabel: eventLabel,
-  });
-  console.log("Event pushed:", eventCategory, eventAction, eventLabel);
-}
+document.addEventListener("DOMContentLoaded", function() {
+  AOS.init({ duration: 1000 });
+
+  function trackEvent(eventCategory, eventAction, eventLabel) {
+    window.dataLayer = window.dataLayer || [];
+    try {
+      window.dataLayer.push({
+        event: "custom_event",
+        eventCategory: eventCategory,
+        eventAction: eventAction,
+        eventLabel: eventLabel,
+      });
+      console.log("Event pushed:", eventCategory, eventAction, eventLabel);
+    } catch (error) {
+      console.error("Error pushing to dataLayer:", error);
+    }
+  }
+});
